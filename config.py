53eae84354415e157c9ff24ea06c7c97c3c5678d
yaelco94 <yaelco94@gmail.com>
2021-03-23 19:01:21 +0200

    fix guassian func

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-SIGMA_R = 16
-SIGMA_D = 12
-DIAMETER = [7]
+SIGMA_R = [10, 50, 100]
+SIGMA_D = [1, 15, 5]
+DIAMETER = [5]
 COLOR = True
--- main.py
+++ main.py
@@ -45,15 +45,15 @@ class BilateralFilter(object):
         in case this neighbor is off the image limits, returns the reflected pixel (in the diameter square)"""
         neighbour_x = x - x_def
         neighbour_y = y - y_def
-        if neighbour_x >= len(self.original):
+        if neighbour_x not in range(len(self.original)):
             neighbour_x += 2 * x_def
-        if neighbour_y >= len(self.original[0]):
+        if neighbour_y not in range(len(self.original[0])):
             neighbour_y += 2 * y_def
         return neighbour_x, neighbour_y
 
     def calculate_single_value(self, neighbor_val, org_val, gaussian_d, sum_value, Wp):
         gaussian_r = self.gaussian(int(neighbor_val) - int(org_val),
-                                   self.sigma_r)
+                                   'r')
         w = gaussian_r * gaussian_d
         sum_value += neighbor_val * w
         Wp += w
@@ -73,11 +73,11 @@ class BilateralFilter(object):
 
     def apply_bilateral_filter(self, x, y):
         half_diameter = int((self.diameter - 1) / 2)
-        sum_filtered_val, Wp = create_filter_and_sum_values()
+        sum_filtered_val, Wp = create_filter_and_sum_values()  # initiate to zero
         for i in range(self.diameter):
             for j in range(self.diameter):
                 neighbour_x, neighbour_y = self.find_neighbours(x, y, (half_diameter - i), (half_diameter - j))
-                gaussian_d = self.gaussian(distance(neighbour_x, neighbour_y, x, y), self.sigma_d)
+                gaussian_d = self.gaussian(distance(neighbour_x, neighbour_y, x, y), 'd')
                 sum_filtered_val, Wp = self.find_filtered_pixel_sum_value(x, y, neighbour_x, neighbour_y, gaussian_d,
                                                                           sum_filtered_val, Wp)
         self.filtered_image[x][y] = sum_filtered_val / Wp
@@ -87,18 +87,20 @@ class BilateralFilter(object):
             print(i)
             for j in range(len(self.original[0])):
                 self.apply_bilateral_filter(i, j)
-                j += 1
-            i += 1
         return self.filtered_image
 
 
 if __name__ == "__main__":
     if COLOR:
-        original_image = cv2.imread(f"{image_path}/05.png")
+        original_image = cv2.imread(f"{image_path}/bricks.png")
     else:
-        original_image = cv2.imread(f"{image_path}/04.png", 0)
+        original_image = cv2.imread(f"{image_path}/dog11.png", 0)
     cv2.imwrite("original_image.png", original_image)
     for diameter in DIAMETER:
-        bilateral_filter = BilateralFilter(original_image, diameter, SIGMA_D, SIGMA_R, COLOR)
-        filtered_image = bilateral_filter.create_bilateral_filter()
-        cv2.imwrite(f"filtered_image_window_{diameter}.png", filtered_image)
+        for r in SIGMA_R:
+            for d in SIGMA_D:
+                bilateral_filter = BilateralFilter(original_image, diameter, d, r, COLOR)
+                filtered_image = bilateral_filter.create_bilateral_filter()
+                print(filtered_image[10][10])
+                print(original_image[10][10])
+                cv2.imwrite(f"filtered_image_{diameter},{d},{r}.png", filtered_image)
